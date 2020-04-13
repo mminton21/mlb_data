a4a5fe3f2e356c20d4c1a6b1285e98b4eb1949f0
Matthew Minton <matthewminton+github@gmail.com>
2020-04-13 23:18:45 +0000

    Iterated through the list of MLB_teams to put the indivudal team ID and team name into a dictionary.

--- mlb_ids.py
+++ mlb_ids.py
@@ -4,4 +4,16 @@ url = "http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_c
 response = requests.request("GET", url)
 
 teams = response.json()
-print(teams)
+teamandid = teams['team_all_season']['queryResults']['row']
+
+#print(teamandid[0]['name_display_full'])
+i = 0
+team_pair = []
+
+while i < len(teamandid):
+   tmname = teamandid[i]['name_display_full']
+   tmid = teamandid[i]['mlb_org_id']
+   team_pair.append({"team_name":tmname, "team_id":tmid})
+   i += 1
+
+print(team_pair)
